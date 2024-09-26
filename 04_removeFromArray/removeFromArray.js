139e8a920b6d515c0b71e94552cbe708bc5c6a85
OrangePoki <113347035+OrangeProfessor@users.noreply.github.com>
2024-09-26 22:56:11 +1000

    04_removeFromArray, passed all tests

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,34 +1,11 @@
-/*const removeFromArray = function(givenArray, ...numsToDelete) {
+const removeFromArray = function(givenArray, ...numsToDelete) {
     let newArray = [];
-    for (const num of numsToDelete) {
-        console.log(num);  //Debugging
-        for (let i=0; i < givenArray.length; i++) {
-            console.log(i); //Debugging
-            if (givenArray[i] === num) {
-                continue;
-            } else {
-                newArray.push(givenArray[i])
-            }
-        }
-
-        }
-    }
-};
-*/
-
-//const removeFromArray = function(givenArray, ...numsToDelete) {
-function removeFromArray (givenArray, ...numsToDelete) {
-    let newArray = [];
-    console.log(numsToDelete);
+    
     for (let i=0; i < givenArray.length; i++) {
-        console.log(`Loop ${i}`); //Debugging
-        console.log(`Number to consider: ${givenArray[i]}`); //Debugging
         if (numsToDelete.includes(givenArray[i])) {
-            console.log(`Removed: ${givenArray[i]}`); //Debugging
             continue;
         } else {
             newArray.push(givenArray[i]);
-            console.log(newArray); //Debugging
         }
     }
 
@@ -38,7 +15,7 @@ function removeFromArray (givenArray, ...numsToDelete) {
     
 
 
-removeFromArray([1, 2, 3], "1", 3);
+//removeFromArray([1, 2, 3], "1", 3);
 
 // Do not edit below this line
 module.exports = removeFromArray;
