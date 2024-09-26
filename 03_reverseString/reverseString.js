d5311746a46b715f6be108b41a6f429254e7542d
OrangePoki <113347035+OrangeProfessor@users.noreply.github.com>
2024-09-26 20:35:07 +1000

    03_reverseString, passed all tests

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    let reversedString = "";
 
+    for (i=string.length; i >= 0; i--) {
+        reversedString = reversedString.concat(string.charAt(i));
+        //console.log(`loop: ${i}, ${reversedString}`);
+    }
+    //console.log(reversedString);
 };
 
 // Do not edit below this line
