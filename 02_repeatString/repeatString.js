a96cf9c66f11ec29c78c3fb8e707c85720b01555
OrangePoki <113347035+OrangeProfessor@users.noreply.github.com>
2024-09-26 14:47:20 +1000

    02_repeatString, passed 4th test

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,19 @@
-const repeatString = function() {
+const repeatString = function(string, number) {
+    let totalString = string;
 
+    if (number === 1) {
+        return totalString;
+    } else if (number === 0) {
+        return;
+    }
+
+    //i = 1 since totalString is already given a value. 
+    //If i = 0, "totalString" will have one more unwanted string
+    for (let i=1; i < number; i++) {
+        totalString = totalString.concat(string);
+    }
+
+    return totalString;
 };
 
 // Do not edit below this line
