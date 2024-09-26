f1c890473bfe9073aef9de816fbc5b171dbeff1f
OrangePoki <113347035+OrangeProfessor@users.noreply.github.com>
2024-09-27 09:26:17 +1000

    05_sumAll, attempt 1, doesn't work as intended lol

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,26 @@
-const sumAll = function() {
+//const sumAll = function(num1, num2) {
+function sumAll(num1, num2) {
+    //sum = lesser number first
+    let sum = num1;
 
+    //Debugging
+    let round = 0;
+    while (sum <= num2) {
+        console.log(`Current round: ${round}`);
+        round++;
+        console.log(`Current sum: ${sum}`);
+
+        sum = sum + (sum + 1);
+
+        console.log(`New sum value: ${sum}`);
+
+
+    }
+
+    return sum;
 };
+
+sumAll(1, 4);
 
 // Do not edit below this line
 module.exports = sumAll;
