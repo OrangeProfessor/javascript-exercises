4fa312b182ea30459d202fca10c98d42c8600e83
OrangePoki <113347035+OrangeProfessor@users.noreply.github.com>
2024-09-28 15:11:28 +1000

    Preparing for full test

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,14 +1,14 @@
-//const sumAll = function(num1, num2) {
+const sumAll = function(...nums) {
 
-function sumAll(...nums) {
+//function sumAll(...nums) {
     const numRange = [];
     let smallestNum = Math.min(...nums);
 
-    for (let num in numRange) {
-        if (!Number.isInteger(num) || num < 0) {
-            console.log("ERROR");
-            //return "ERROR";
-            return;
+    for (let i=0; i < nums.length; i++) {
+        if (!Number.isInteger(nums[i]) || nums[i] < 0) {
+            //console.log("ERROR");
+            return "ERROR";
+            //return;
         }
     }
 
@@ -20,26 +20,29 @@ function sumAll(...nums) {
     }
 
     let arrayLength = numRange.length;
-    console.log(`numRange length: ${arrayLength}`);
+    //console.log(`numRange length: ${arrayLength}`);
 
     //Sum all numRange elements
     for (let i=1; i < arrayLength; i++) {
         //Debugging
+        /*
         console.log(`Current "i" value: ${i}`);
         console.log(`Current numRange[0] value: ${numRange[0]}`);
         console.log(`Current numRange[1] value: ${numRange[1]}`);
+        */
 
         numRange[0] = numRange[0] + numRange[1];
         numRange.splice(1, 1); //Remove second element
 
-        console.log(`New numRange[0] value: ${numRange[0]}`);
-        console.log(`New numRange[1] value: ${numRange[1]}`);
+        //console.log(`New numRange[0] value: ${numRange[0]}`);
+        //console.log(`New numRange[1] value: ${numRange[1]}`);
     }
   
-    console.log(`Sum: ${numRange}`);
+    //console.log(`Sum: ${numRange}`);
+    return numRange;
 };
 
-sumAll(-10, 4);
+sumAll(1, 4);
 
 // Do not edit below this line
 module.exports = sumAll;
