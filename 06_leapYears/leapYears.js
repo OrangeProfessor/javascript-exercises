49a0ec4541a194980231d8523e1f1da0cbe0dc98
OrangePoki <113347035+OrangeProfessor@users.noreply.github.com>
2024-09-30 08:22:59 +1000

    Made neater

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,14 +1,7 @@
 const leapYears = function(num) {
-    /* Debugging: 
-    console.log(`num % 4: ${num % 4}`);
-    console.log(`num % 4: ${num % 400}`);
-    */
-
     if (num % 4 === 0) {
-        //If num is divisible by 100 AND is divisible by 400
         if (num % 100 === 0 && num % 400 === 0) {
             return true;
-        //Else if num is divisible by 100 AND is NOT divisible by 400
         } else if (num % 100 === 0 && num % 400 != 0) {
             return false;
         }
