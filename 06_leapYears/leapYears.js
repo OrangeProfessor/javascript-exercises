9831d52dfbb79acf3631c0562b8be8c49f2f332a
OrangePoki <113347035+OrangeProfessor@users.noreply.github.com>
2024-09-30 01:23:03 +1000

    passed first test

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,17 @@
-const leapYears = function() {
-
+const leapYears = function(num) {
+    console.log(`num % 4: ${num % 4}`);
+    console.log(`num % 4: ${num % 400}`);
+    if (num % 4 === 0) {
+        if (num % 100 === 0 && num % 400 === 0) {
+            return true;
+        } else if (num % 100 === 0 && num % 400 != 0) {
+            return false;
+        }
+        //If num !>= 100
+        return true;
+    } else {
+        return false;
+    }
 };
 
 // Do not edit below this line
