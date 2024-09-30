cc88a91e9a3188f8c1f0f79c2924fec893d90e87
OrangePoki <113347035+OrangeProfessor@users.noreply.github.com>
2024-09-30 11:38:54 +1000

    passed test 1

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(f) {
+  let celsius = (f-32)*(5/9);
+  return celsius;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(c) {
+  let fahr = c*(9/5) + 32;
+  return fahr;
 };
 
 // Do not edit below this line
