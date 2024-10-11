f63b956af881b9227027f782b40e2b25e66d2744
OrangePoki <113347035+OrangeProfessor@users.noreply.github.com>
2024-10-12 09:16:30 +1000

    Passed all tests

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,23 +1,23 @@
 const fibonacci = function(input) {
 const num = Number(input);
 
-//Fibonacci of 1 and 0 is = 1
-if (num === 1 || num === 0) return 1;
+//Fibonacci of 0 is 0
+if (num === 0) return 0;
 
 //Error check
 if (num < 0) return "OOPS";
 
-const sequence = [1,1];
+const sequence = [0, 1];
 
-for (let i=1; i < num-1; i++) {
+for (let i=1; i < num; i++) {
     sequence.push(sequence[i] + sequence[i-1]);
 }
 //console.log(sequence);
-return sequence[num-1];
+return sequence[num];
 
 };
 
-console.log(fibonacci(-25));
+//console.log(fibonacci(4));
 
 // Do not edit below this line
 module.exports = fibonacci;
