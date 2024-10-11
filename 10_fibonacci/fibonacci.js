5aaa7482affa5ba8075e2c9720989c25ca5e42a3
OrangePoki <113347035+OrangeProfessor@users.noreply.github.com>
2024-10-12 09:01:09 +1000

    Added error check

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,18 +1,23 @@
-const fibonacci = function(num) {
-//Make functionality where it adds a fibonacci number...
-//based on the input
+const fibonacci = function(input) {
+const num = Number(input);
+
+//Fibonacci of 1 and 0 is = 1
+if (num === 1 || num === 0) return 1;
+
+//Error check
+if (num < 0) return "OOPS";
+
 const sequence = [1,1];
 
 for (let i=1; i < num-1; i++) {
-    //if (isNaN(sequence[i-1])) sequence.push(1);
     sequence.push(sequence[i] + sequence[i-1]);
 }
-console.log(sequence);
+//console.log(sequence);
 return sequence[num-1];
 
 };
 
-console.log(fibonacci(6));
+console.log(fibonacci(-25));
 
 // Do not edit below this line
 module.exports = fibonacci;
