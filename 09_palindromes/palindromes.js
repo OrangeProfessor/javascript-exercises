32dbd473764a97c96c9061629f9bb9cae3955595
OrangePoki <113347035+OrangeProfessor@users.noreply.github.com>
2024-10-11 17:35:47 +1000

    Completed "letters only" functionality

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,22 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    //Defines the symbols that should be excluded from string comparisons
+    const unwanted = [" ", ",", "."]
 
+    string = Array.from(string);
+    const lettersOnly = string.filter(item => !unwanted.includes(item))
+        .reduce((allString, curItem) => {
+            //Note: have to have a return statement for .reduce()
+            return allString + curItem;
+        }).toLowerCase();
+    return lettersOnly;
+    /*
+    for (let i=string.length; i >= 0; i--) {
+
+    }
+    */
 };
+
+console.log(palindromes("Animal loots foliated detail of stool lamina."))
 
 // Do not edit below this line
 module.exports = palindromes;
