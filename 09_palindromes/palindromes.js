4f29465dd85c0a9a0247137874b928cb3271d26b
OrangePoki <113347035+OrangeProfessor@users.noreply.github.com>
2024-10-11 18:07:40 +1000

    Didn't pass test 2 and 3

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -3,20 +3,31 @@ const palindromes = function (string) {
     const unwanted = [" ", ",", "."]
 
     string = Array.from(string);
-    const lettersOnly = string.filter(item => !unwanted.includes(item))
-        .reduce((allString, curItem) => {
-            //Note: have to have a return statement for .reduce()
-            return allString + curItem;
-        }).toLowerCase();
-    return lettersOnly;
-    /*
-    for (let i=string.length; i >= 0; i--) {
 
-    }
-    */
+    const lettersOnlyArr = string.filter(item => !unwanted.includes(item));
+
+    const strToCompare = lettersOnlyArr.reduce((allString, curItem) => {
+        //Note: have to have a return statement for .reduce()
+        return allString + curItem;
+        }).toLowerCase();
+    
+    const revString = lettersOnlyArr.reverse().reduce((allString, curItem) => {
+        return allString + curItem;
+        }).toLowerCase();
+
+    //console.log(strToCompare);
+    //console.log(revString);
+
+    return strToCompare === revString;
+
+    /* NOTE: The function was returning "undefined" since it wasn't returning something.
+    While I was using two console.logs() to print out the values of strToCompare and 
+    revString, the "undefined" value was still showing on the console output since the 
+    function still had to return something, but there wasn't anything to return with.
+    Having that last return statement alleviated the issue; */
 };
 
-console.log(palindromes("Animal loots foliated detail of stool lamina."))
+//console.log(palindromes("Animal loots foliated detail of stool lamina."))
 
 // Do not edit below this line
 module.exports = palindromes;
