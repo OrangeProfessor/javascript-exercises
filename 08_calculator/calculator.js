563642d1a08e0a894aeda1dbcb743bf80158edc9
OrangePoki <113347035+OrangeProfessor@users.noreply.github.com>
2024-10-11 16:48:50 +1000

    Exercise 8 completed

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,33 @@
-const add = function() {
-	
+const add = function(...nums) {
+	return nums.reduce((sum, numToAdd) => sum + numToAdd);
 };
 
-const subtract = function() {
-	
+const subtract = function(...nums) {
+  return nums.reduce((diff, numToSub) => diff - numToSub);
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	return array.reduce(((sum, numToAdd) => sum + numToAdd), 0);
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  return array.reduce((product, numToMul) => product * numToMul);
 };
 
-const power = function() {
-	
+const power = function(num, powerOf) {
+	const array = [];
+
+  for (let i=0; i < powerOf; i++) array.push(num);
+
+  return array.reduce((product, numToMul) => product * numToMul)
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	const factorial = [];
+
+  for (let i=num; i > 0; i--) factorial.push(i);
+
+  return num === 0 ? 1 : factorial.reduce((product, numToMul) => product * numToMul);
 };
 
 // Do not edit below this line
