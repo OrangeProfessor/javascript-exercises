2f4edb053d1e69d4708ffa2debf418a50d8e1548
OrangePoki <113347035+OrangeProfessor@users.noreply.github.com>
2024-10-12 08:52:55 +1000

    Passed 5 tests

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,18 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+//Make functionality where it adds a fibonacci number...
+//based on the input
+const sequence = [1,1];
+
+for (let i=1; i < num-1; i++) {
+    //if (isNaN(sequence[i-1])) sequence.push(1);
+    sequence.push(sequence[i] + sequence[i-1]);
+}
+console.log(sequence);
+return sequence[num-1];
 
 };
+
+console.log(fibonacci(6));
 
 // Do not edit below this line
 module.exports = fibonacci;
