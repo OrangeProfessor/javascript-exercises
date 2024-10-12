66825138d496e5f6aaee57932267751e708e6d03
OrangePoki <113347035+OrangeProfessor@users.noreply.github.com>
2024-10-12 12:56:06 +1000

    I think I should change the array to only include the oldest person

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -15,13 +15,18 @@ const set = [
       yearOfDeath: 1941,
     },
 ]
-/*
+
 const findTheOldest = function(people) {
-    people.sort(person => {
-        if (!person.ye)
+    people.sort((personA, personB) => {
+        if (!personA.yearOfDeath) personA.yearOfDeath = 2024;
+        if (!personB.yearOfDeath) personB.yearOfDeath = 2024;
+
+        return (personA.yearOfDeath - personA.yearOfBirth) - 
+        (personB.yearOfDeath - personB.yearOfBirth);
+        
     })
+    return people[people.length - 1].name;
 };
-*/
 
 /*
 const findTheOldest = function(people) {
@@ -38,7 +43,7 @@ const findTheOldest = function(people) {
 }
 */
 
-console.log(findTheOldest(set));
+//console.log(findTheOldest(set));
 
 // Do not edit below this line
 module.exports = findTheOldest;
